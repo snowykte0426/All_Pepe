3a3d6dbe371d229e5c17f806bfee74998aba3921
snowykte0426 <snowykte0426@naver.com>
2024-06-25 11:39:20 +0900

    Pepe!

---
+++ pepe_/index.js
@@ -0,0 +1,6 @@
+setInterval(()=>{
+    let imgs = document.querySelectorAll('img');
+    imgs.forEach((a,i)=>{
+        a.src = 'https://www.google.co.kr/url?sa=i&url=https%3A%2F%2Fnamu.wiki%2Fw%2F%25ED%258E%2598%25ED%258E%2598%2520%25EB%258D%2594%2520%25ED%2594%2584%25EB%25A1%259C%25EA%25B7%25B8&psig=AOvVaw3x0GjUkcY6reU5wUWxv13I&ust=1719366359979000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKCU0uva9YYDFQAAAAAdAAAAABAN'
+    })
+}, 500)
